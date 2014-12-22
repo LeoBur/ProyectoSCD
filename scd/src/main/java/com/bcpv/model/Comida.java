d3c49d491329f9c7db5a0af695a72d78e3fafef0
Leo <leo_bur@hotmail.com>
2014-12-21 21:01:18 -0300

    Leo - Anda el saveDieta y cascade

--- scd/src/main/java/com/bcpv/model/Comida.java
+++ scd/src/main/java/com/bcpv/model/Comida.java
@@ -1,6 +1,8 @@
 package com.bcpv.model;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -8,36 +10,44 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 import org.hibernate.search.annotations.Indexed;
 
+import static javax.persistence.GenerationType.IDENTITY;
+
 @Entity
-@Table(name="comida")
-@Indexed
+@Table(name = "comida", catalog = "scd", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "idComida")})
 public class Comida implements Serializable{
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 7379603373775505732L;
-	
+
 	private Long idComida;
 	private String cantidad;
 	private Alimento alimento;
 	private String observaciones;
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	@Column(name="idComida")
+    private Set<MomentoDia> momentos = new HashSet<MomentoDia>(0);
+
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+	@Column(name="idComida", unique = true, nullable = false)
 	public Long getIdComida() {
 		return idComida;
 	}
 	public void setIdComida(Long idComida) {
 		this.idComida = idComida;
 	}
-	
+
 	@Column(name="cantidad",nullable=false)
 	public String getCantidad() {
 		return cantidad;
@@ -45,8 +55,9 @@ public class Comida implements Serializable{
 	public void setCantidad(String cantidad) {
 		this.cantidad = cantidad.toUpperCase();
 	}
-	@ManyToOne(fetch=FetchType.LAZY)
-	@JoinColumn(name="idAlimento",nullable=false)
+	@ManyToOne(fetch=FetchType.EAGER)
+    @Fetch(FetchMode.SELECT)
+	@JoinColumn(name="idAlimento")
 	public Alimento getAlimento() {
 		return alimento;
 	}
@@ -60,4 +71,13 @@ public class Comida implements Serializable{
 	public void setObservaciones(String observaciones) {
 		this.observaciones = observaciones.toUpperCase();
 	}
+
+    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "comidas")
+    public Set<MomentoDia> getMomentos() {
+        return momentos;
+    }
+
+    public void setMomentos(Set<MomentoDia> momentos) {
+        this.momentos = momentos;
+    }
 }
--- scd/src/main/java/com/bcpv/model/MomentoDia.java
+++ scd/src/main/java/com/bcpv/model/MomentoDia.java
@@ -1,11 +1,17 @@
 package com.bcpv.model;
 
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -13,32 +19,38 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import static javax.persistence.GenerationType.IDENTITY;
 
 @Entity
-@Table(name="momento_dia")
+@Table(name = "momento_dia", catalog = "scd", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "idMomentoD")})
 public class MomentoDia implements Serializable{
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 6002754156113557807L;
-	
-	public enum MomentosDia {DESAYUNO,MEDIA_MANIANA,ALMUERZO,MEDIA_TARDE,CENA,ANTES_DE_ACOSTARSE } 
-	
+
+	public enum MomentosDia {DESAYUNO,MEDIA_MANIANA,ALMUERZO,MEDIA_TARDE,CENA,ANTES_DE_ACOSTARSE }
+
 	private Long idMomentoD;
 	private MomentosDia nombre;
 	private Set<Comida> comidas = new HashSet<Comida>();
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	@Column(name="idMomentoD")
+    private Set<DiaDieta> diaDietas = new HashSet<DiaDieta>(0);
+
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+	@Column(name="idMomentoD", unique = true, nullable = false)
 	public Long getIdMomentoD() {
 		return idMomentoD;
 	}
 	public void setIdMomentoD(Long idMomentoD) {
 		this.idMomentoD = idMomentoD;
 	}
-	
+
 	@Column(name="nombre_momento")
 	public MomentosDia getNombre() {
 		return nombre;
@@ -46,14 +58,26 @@ public class MomentoDia implements Serializable{
 	public void setNombre(MomentosDia nombre) {
 		this.nombre = nombre;
 	}
-	
-	@ManyToMany(cascade={})
-	@JoinTable(name="momento_dia_comidas", joinColumns={@JoinColumn(name="idMomentoD")},inverseJoinColumns={@JoinColumn(name="idComida")})
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @Fetch(FetchMode.SELECT)
+    @Cascade(org.hibernate.annotations.CascadeType.ALL)
+    @JoinTable(name = "momento_dia_comidas",
+            joinColumns = { @JoinColumn(name = "idMomentoD") },
+            inverseJoinColumns = { @JoinColumn(name = "idComida") })
 	public Set<Comida> getComidas() {
 		return comidas;
 	}
 	public void setComidas(Set<Comida> comidas) {
 		this.comidas = comidas;
 	}
-	
+
+    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "momentosDia")
+    public Set<DiaDieta> getDiaDietas() {
+        return diaDietas;
+    }
+
+    public void setDiaDietas(Set<DiaDieta> diaDietas) {
+        this.diaDietas = diaDietas;
+    }
 }
